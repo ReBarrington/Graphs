f2144a1903732a29dc3227c32417b51efeee7137
Reagan Barrington <reagan@barringtonfamily.net>
2020-08-11 17:39:25 -0400

    first pass

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,6 +1,9 @@
+from util import Stack
 
 def earliest_ancestor(ancestors, starting_node):
     relationships = {}
+    s = Stack()
+    visited = set()
 
     # set up relationships dictionary
     for tup in ancestors:
@@ -13,16 +16,31 @@ def earliest_ancestor(ancestors, starting_node):
         # else if child already has a parent, add another parent
         elif child in relationships:
             relationships[child].append(parent)
+
+    print(relationships, ' RELATIONSHIPS')
     
     if starting_node not in relationships.keys():
+        # not a key, means no parents
         return -1
 
-    while starting_node in relationships.keys():
-        # starting-node exists as a child... Means parent also exists.
+    else:
 
-        for starting_node_parent in relationships.get(starting_node):
-            starting_node = starting_node_parent
-            continue
+        # stack the starting node
+        s.push(starting_node)
 
-    return starting_node
+        while s.size() > 0:
+            top_node = s.pop()
+
+            if top_node not in visited:
+                visited.add(top_node)
+
+                print(top_node, ' has been visited.')
+
+                # if top node has parents
+                if top_node in relationships.keys():
+                    for parent_of_top_node in relationships.get(top_node):
+                        s.push(parent_of_top_node)
+
+        return top_node
     
+
