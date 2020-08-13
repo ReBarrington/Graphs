817405e1b84df514b8b81d6f3c9fc193e8752ecb
Reagan Barrington <reagan@barringtonfamily.net>
2020-08-13 19:15:35 -0400

    day 1 of sprint

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -30,32 +30,72 @@ player = Player(world.starting_room)
 traversal_path = ['n']
 
 q_of_paths = Queue()
+q_of_paths.enqueue(traversal_path)
 
 # TRAVERSAL TEST
 visited_rooms = set()
 traversal_graph = dict()
 
 player.current_room = world.starting_room
-visited_rooms.add(player.current_room)
 
-for move in traversal_path:
-    print(f'in  {player.current_room.name}')
-    print(player.current_room.get_exits())
-    print(f'{player.current_room.get_room_in_direction(move).name} is {move} of {player.current_room.name}')
+print(f'in  {player.current_room.name}')
+print(f'exits: {player.current_room.get_exits()}')
 
-    traversal_graph[player.current_room.id] = dict()
+while len(visited_rooms) < len(room_graph):
 
-    for exit in player.current_room.get_exits():
-        if exit == move:
-            traversal_graph[player.current_room.id][exit] = player.current_room.get_room_in_direction(move).id
-        else:
+
+    if player.current_room.name not in visited_rooms:
+        print("Haven't visited this room before. Initializing: ")
+        visited_rooms.add(player.current_room.name)
+
+        traversal_graph[player.current_room.id] = dict()
+
+        for exit in player.current_room.get_exits():
+            # Initialize all directions as "?" as this room has not been visited.
             traversal_graph[player.current_room.id][exit] = "?"
-       
 
-    print(traversal_graph, ' TRAVERSAL GRAPH')
-    
-    traversal_path.append(move)
-    player.travel(move)
+        try: 
+            traversal_graph[player.current_room.id][direction_of_prev] = prev_room.id
+        except:
+            continue
+
+        print(traversal_graph)
+
+    for move in traversal_path:
+
+        if player.current_room.get_room_in_direction(move) is not None:
+            print(' Can move ', move, ' to ', player.current_room.get_room_in_direction(move).name)
+
+            prev_room = player.current_room
+            direction_of_prev = player.current_room.return_opposite_direction(move)
+
+            traversal_graph[player.current_room.id][move] = player.current_room.get_room_in_direction(move).id
+            print(' UPDATING INFO: ', traversal_graph)
+            print('moving ', move)
+            player.travel(move)
+
+            # save prev room to this room's info
+            print(' prev room was ', prev_room.id, '. Should be recorded as ', direction_of_prev)
+            # traversal_graph[player.current_room.id][direction_of_prev] = prev_room_id
+
+            continue
+
+
+        # When cannot move in that direction, pick first "?" from dictionary
+        print(" CANNOT CONTINUE MOVING ", move)
+        
+        print(player.current_room.id, ' current room')
+        print(traversal_graph[player.current_room.id].values())
+        for key, value in (traversal_graph[player.current_room.id].items()):
+            if value == "?":
+                print(key,' will be new direction')
+                traversal_path.append(key)
+        else:
+            print('NO UNKNOWN EXITS..')
+            print(traversal_path, ' traversal path')
+            q_of_paths.enqueue(traversal_path)
+            debugger 
+
 
 if len(visited_rooms) == len(room_graph):
     print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
