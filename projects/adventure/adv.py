8ca0b450b526b8b6bd56cacacfe55ed99e845262
Reagan Barrington <reagan@barringtonfamily.net>
2020-08-14 10:47:12 -0400

    mvp

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,21 +1,36 @@
 from room import Room
 from player import Player
 from world import World
+from util import Queue
 
 import random
-from util import Queue
 from ast import literal_eval
 
 # Load world
 world = World()
 
+def initialize_room(current_room):
+    print(f' Initializing {current_room.name}')
+    visited_rooms.add(current_room.name)
+    mapped_dict[current_room.id] = dict()
+
+    for exit in current_room.get_exits():
+        # Initialize all directions as "?" as this room has not been visited.
+        mapped_dict[current_room.id][exit] = "?"
+
+    try: 
+        mapped_dict[current_room.id][direction_of_prev] = prev_room.id
+    except:
+        pass
+
+    print(mapped_dict)
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -27,68 +42,49 @@ world.print_rooms()
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
-traversal_path = ['n']
+# traversal_path = ['n', 'n']
+traversal_path = []
 
-q = Queue()
-q.enqueue([traversal_path])
+
 
 # TRAVERSAL TEST
 visited_rooms = set()
 mapped_dict = dict()
-
 player.current_room = world.starting_room
+visited_rooms.add(player.current_room)
 
-def initialize_room():
-    print(f' Initializing {player.current_room.name}')
-    visited_rooms.add(player.current_room.name)
-    mapped_dict[player.current_room.id] = dict()
+q = Queue()
+q.enqueue(player.current_room)
 
-    for exit in player.current_room.get_exits():
-        # Initialize all directions as "?" as this room has not been visited.
-        mapped_dict[player.current_room.id][exit] = "?"
+# for move in traversal_path:
+# while q.size() > 0:
+while len(room_graph) > len(visited_rooms):
 
-    try: 
-        mapped_dict[player.current_room.id][direction_of_prev] = prev_room.id
-    except:
-        pass
+    # let current_room be equal to q.dequeue
+    current_room = q.dequeue()
 
-    print(mapped_dict)
+    print(f'CURRENT ROOM: {current_room}')
 
+    if current_room.name not in visited_rooms:
+        initialize_room(current_room)
 
-while q.size() > 0:
+        for exit in current_room.get_exits():
 
-    last_direction = q.dequeue()
-
-    print(f'in  {player.current_room.name}')
-    print(f'exits: {player.current_room.get_exits()}')
-    print(f' visited:  {visited_rooms}' )
-
-    if player.current_room.name not in visited_rooms:
-        initialize_room()
-
-    # for each possible direction
-    for key, value in (mapped_dict[player.current_room.id].items()):
-
-        if value == "?":
-            print(key,' is unexplored.')
-            move = key
-
-            prev_room = player.current_room
-            direction_of_prev = player.current_room.return_opposite_direction(move)
-
-            mapped_dict[player.current_room.id][move] = player.current_room.get_room_in_direction(move).id
+            mapped_dict[current_room.id][exit] = current_room.get_room_in_direction(exit).id
             print(' UPDATING INFO: ', mapped_dict)
 
-            q.enqueue(move)
-            print('moving ', move)
-            player.travel(move)
-            
-            if player.current_room.name not in visited_rooms:
-                break
+            prev_room = player.current_room
+            direction_of_prev = player.current_room.return_opposite_direction(exit)
 
+            traversal_path.append(exit)
+            q.enqueue(current_room.get_room_in_direction(exit))
 
-    print("No unexplored directions in this room.")
-    continue
+            print('moving ', exit)
+            player.travel(exit)
+
+            print(q.queue, ' is queue')
+            # print(traversal_path, ' is traversal path')
+
 
 
 if len(visited_rooms) == len(room_graph):
@@ -96,6 +92,7 @@ if len(visited_rooms) == len(room_graph):
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
+
 
 
 
